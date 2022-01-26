a88dfcf6dcbf67e717f675302e71316d2da212cb
maney hatake <iamshivang@outlook.com>
2022-01-26 10:26:43 +0530

    ver

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -11,10 +11,14 @@ const getClient = async (req, reply) => {
 }
 const getAllClient = async (req, reply) => {
   const client = await Client.find().populate('in_cart.item').lean().exec()
-
   reply.send({ client })
 }
 const addClient = async (req, reply) => {
+  let a = await Client.find({ mail: req.body.mail }).lean().exec()
+  if (a.length != 0) {
+    reply.send('Already Exist')
+    return
+  }
   const client = await Client.create(req.body)
   reply.send({ client })
 }
@@ -108,6 +112,11 @@ const purchaseAll = async (req, reply) => {
     .populate('in_cart.item liked.item purchased.item.item')
     .lean()
     .exec()
+if (client[0].in_cart==0) {
+  reply.send("Cart Empty")
+  return
+}
+  const pr = await Product.find().lean().exec()
 
   let date = new Date()
   let Cd = new Date().toString().split(' ')
@@ -116,6 +125,8 @@ const purchaseAll = async (req, reply) => {
   let Price = 0
   client[0].in_cart.map((a) => {
     Price += a.item.price * a.count
+    // let b = pr.filter((x) => x._id.toString() == a.item._id)
+    // console.log(b);
   })
 
   let O = {
